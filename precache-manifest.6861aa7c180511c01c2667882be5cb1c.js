828a7e3aa06d0cda045be6c602e8604512509c03
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2018-12-18 23:24:30 +0800

    Updates

---
+++ precache-manifest.6861aa7c180511c01c2667882be5cb1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8507572375d5b5ea7f9",
+    "url": "/random-quote-machine/static/js/runtime~main.b8507572.js"
+  },
+  {
+    "revision": "6f6f4239776524ddb86f",
+    "url": "/random-quote-machine/static/js/main.6f6f4239.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/random-quote-machine/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "6f6f4239776524ddb86f",
+    "url": "/random-quote-machine/static/css/main.4fdc6caf.chunk.css"
+  },
+  {
+    "revision": "52c502064aca259ba54cdf5bf9ba36c4",
+    "url": "/random-quote-machine/index.html"
+  }
+];
